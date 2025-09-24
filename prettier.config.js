41ae55abcd18ba4262db3f88651ef6ba6d8b6b19
Alex Winkler <alexander.winkler8@gmail.com>
2025-09-24 17:05:13 +0200

    added lint, prettier and husky

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: false, // kein ; am Zeilenende
+  singleQuote: true, // ' statt "
+  tabWidth: 2, // 2 Leerzeichen pro Tab
+  trailingComma: 'es5', // bei Objekten/Arrays Komma am Ende, wo möglich
+}
